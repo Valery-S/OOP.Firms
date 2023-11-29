1896c252f645fdad844b8f2da8fb52a259810c63
Valery-S <valerij-01@mail.ru>
2023-11-29 19:10:44 +0300

    Add test project

---
+++ TestsForFirms/ShuffleExtension.cs
@@ -0,0 +1,11 @@
+﻿public static class ShuffleExtension
+{
+	public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> items)
+	{
+		if (items == null) throw new ArgumentNullException(nameof(items));
+
+		var rnd = new Random();
+
+		return items.OrderBy(_ => rnd.Next());
+	}
+}
